0a956a18c9b2c3c93ab5299f1258f99039f14a0f
Harry Buck <harry-buck@hotmail.co.uk>
2019-07-30 19:52:32 +0100

    Removed redundant imports

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
 import DiscussionBar from './components/DiscussionBar';
-
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PostPane from './components/PostPane'
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <DiscussionBar/>
+        <PostPane/>
 
     </div>
   );
